c49a8a241db9f6b98c8f9b93cc8539381fdd6667
Tadeusz Makuch <Tadeusz Makuch>
2017-01-23 20:43:03 +0100

    Added CORS support to projects server.

--- Project/server/server.js
+++ Project/server/server.js
@@ -11,6 +11,13 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.text());
 app.use(bodyParser.json());
 
+//CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/api', require('./routers/api'));
 
 app.listen(8081, function() {
